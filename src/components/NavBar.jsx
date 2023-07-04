9ee7ee040ae84199013f19fadb054327fee5148e
Job Muturi <muirimuturi17@gmail.com>
2023-07-04 20:40:09 +0300

    Added Basic NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+import React, {} from "react";
+import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import HomePage from "../pages/HomePage";
+import Recipes from "../pages/Recipes";
+
+function NavBar() {
+
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/" element ={<HomePage/>}>Home</Nav.Link>
+                <Nav.Link href="recipes" element={<Recipes/>}>Recipes</Nav.Link>
+                <Nav.Link href="chefs">Chefs</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      );
+}
+export default NavBar;
