4a40fb2a91a7338374fed89b9309c2fc0366932e
Job Muturi <muirimuturi17@gmail.com>
2023-07-04 13:01:25 +0300

    Updated login form

---
+++ src/components/SignUpFrom.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import Error from "../styles/Error";
+
+function SignUpForm({ onLogin }) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+    const [bio, setBio] = useState("");
+    const [errors, setErrors] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+  
+    function handleSubmit(e) {
+      e.preventDefault();
+      setErrors([]);
+      setIsLoading(true);
+      fetch("/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          password,
+          password_confirmation: passwordConfirmation,
+          image_url: imageUrl,
+          bio,
+        }),
+      }).then((r) => {
+        setIsLoading(false);
+        if (r.ok) {
+          r.json().then((user) => onLogin(user));
+        } else {
+          r.json().then((err) => setErrors(err.errors));
+        }
+      });
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="form_main">
+              <p class="heading">Sign Up</p>
+    <p>Username</p>
+    <div class="inputContainer">
+        <svg viewBox="0 0 16 16" fill="#2e2e2e" height="16" width="16" xmlns="http://www.w3.org/2000/svg" class="inputIcon">
+        <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"></path>
+        </svg>
+    <input 
+    placeholder="Must be at least 6 characters" 
+    id="username" 
+    class="inputField"
+    autoComplete="off"
+    value={username}
+    onChange={(e) => setUsername(e.target.value)}
+     type="text"
+     />
+    </div>
+
+    <p>Password</p>
+<div class="inputContainer">
+    <svg viewBox="0 0 16 16" fill="#2e2e2e" height="16" width="16" xmlns="http://www.w3.org/2000/svg" class="inputIcon">
+    <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"></path>
+    </svg>
+    <input 
+    placeholder="Include uppercase, lowercase and numbers" 
+    id="password" 
+    class="inputField" 
+    type="password"
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}
+    />
+</div>
+
+<div class="inputContainer">
+    <svg viewBox="0 0 16 16" fill="#2e2e2e" height="16" width="16" xmlns="http://www.w3.org/2000/svg" class="inputIcon">
+    <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"></path>
+    </svg>
+    <input 
+    placeholder="Confirm Password" 
+    id="password-confirmation" 
+    class="inputField" 
+    type="password"
+    value={passwordConfirmation}
+    onChange={(e) => setPassword(e.target.value)}
+    />
+</div>
+
+<p>Profile Image</p>
+<div class="inputContainer">
+    <svg viewBox="0 0 512 512" fill="#2e2e2e" height="16" width="16" xmlns="http://www.w3.org/2000/svg" class="inputIcon">
+    <path d="M448 80c8.8 0 16 7.2 16 16V415.8l-5-6.5-136-176c-4.5-5.9-11.6-9.3-19-9.3s-14.4 3.4-19 9.3L202 340.7l-30.5-42.7C167 291.7 159.8 288 152 288s-15 3.7-19.5 10.1l-80 112L48 416.3l0-.3V96c0-8.8 7.2-16 16-16H448zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm80 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z"/>
+    </svg>
+    <input 
+    placeholder="Link profile image url" 
+    id="password-confirmation" 
+    class="inputField" 
+    type="password"
+    value={passwordConfirmation}
+    onChange={(e) => setPassword(e.target.value)}
+    />
+</div>
+           
+<button id="button" type="submit">
+{isLoading ? "Loading..." : "Sign Up"}  </button>
+   
+    {errors.map((err) => (
+          <Error key={err}>{err}</Error>
+        ))}
+        </form>
+      )
+        
+}
+export default SignUpForm;
