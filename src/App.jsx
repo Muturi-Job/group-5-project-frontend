ffc2a4d2dc3bcd0f91a6859ff524bf1821ea2375
Job Muturi <muirimuturi17@gmail.com>
2023-07-05 11:39:30 +0300

    Updated user effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (!user) return <Login onLogin={setUser} />;
 
   return (
     <>
+
  
     </>
   )
