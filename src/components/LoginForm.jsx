f33aa4952bb0a2358f09d8ca8eea172a3c33ba22
Job Muturi <muirimuturi17@gmail.com>
2023-07-09 17:11:34 +0300

    Updated navbar & userprofile section

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -21,7 +21,7 @@ function LoginForm ({onLogin}) {
       .then((r) => {
         setIsLoading(false);
         if (r.ok) {
-          r.json().then((user) => console.log(user));//onLogin(user));
+          r.json().then((user) => onLogin(user));
           navigate('/home');
         } else {
           throw new Error("Login failed!");
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,12 @@
 import React, {} from "react";
-import { Link } from "react-router-dom";
+import { Link , NavLink} from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import HomePage from "../pages/HomePage";
 import Recipes from "../pages/Recipes";
+import Reviews from "../pages/Reviews";
+import UserProfile from "../pages/UserProfile";
 
 function NavBar({ user, setUser }) {
   function handleLogoutClick() {
@@ -18,14 +20,22 @@ function NavBar({ user, setUser }) {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
           <Container>
-            <Navbar.Brand className="navbar-brand"><Link to='/home'  element ={<HomePage/>} >Culinary Experience</Link></Navbar.Brand>
+            <Navbar.Brand className="navbar-brand"><Link to='/home'  element ={<HomePage/>} >
+            <img className="navbar-logo-image"  src="./images/savortastic-logo.png" alt="" />
+              </Link></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Link to="home" className='nav-link' element ={<HomePage/>}>Home</Link>
-                <Link to="recipes" className='nav-link' element={<Recipes/>}>Recipes</Link>
-                <Link to="users" className='nav-link'>Users</Link>
-                <button variant="outline" onClick={handleLogoutClick} className="logout-btn">Logout</button>
+                {/* <NavLink to="home" className='nav-link' element={<HomePage/>}>Home</NavLink> */}
+        <NavLink to="recipes" className='nav-link' element={<Recipes/>}>Recipes</NavLink>
+        <NavLink to="users" className='nav-link'>Users</NavLink>
+        <NavLink to="reviews" className='nav-link' element={<Reviews/>}>Reviews</NavLink>
+
+        <NavLink to="profile" className='nav-link' element={<UserProfile/>}>
+                {/* <button variant="outline"  className="logout-btn"> */}
+                <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
+                </NavLink>
+                {/* </button> */}
               </Nav>
             </Navbar.Collapse>
           </Container>
